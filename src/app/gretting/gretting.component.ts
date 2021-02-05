a5634d57c88af7b74e949e7886202fc59ed8d764
YamidDev <yamidcuetomazo@gmail.com>
2021-02-05 14:09:09 -0500

    update changes

---
+++ src/app/gretting/gretting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gretting',
+  templateUrl: './gretting.component.html',
+  styleUrls: ['./gretting.component.css']
+})
+export class GrettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
